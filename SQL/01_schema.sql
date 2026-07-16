6a68bd25d906a760dfa5ba77203727f758648b2c
Dare215 <dariousbrown3@icloud.com>
2026-07-16 16:48:41 -0400

    Delete SQL/01_schema.sql

--- SQL/01_schema.sql
+++
@@ -1,5 +0,0 @@
--- Tables
-CREATE TABLE sites(site_id SERIAL PRIMARY KEY, site_name TEXT);
-CREATE TABLE patients(patient_id SERIAL PRIMARY KEY, site_id INT REFERENCES sites(site_id), treatment_arm TEXT, enrolled_date DATE);
-CREATE TABLE visits(visit_id SERIAL PRIMARY KEY, patient_id INT REFERENCES patients(patient_id), visit_date DATE, visit_type TEXT);
-CREATE TABLE adverse_events(ae_id SERIAL PRIMARY KEY, patient_id INT REFERENCES patients(patient_id), severity TEXT, event_date DATE);
